5da2f4e941a32bb497162abeb7ba201e160fbb37
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2015-02-02 10:53:56 +0000

    Remove switch statement

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -84,40 +84,6 @@ public class Translator {
             return null;
 
         String ins = scan();
-        /*
-        switch (ins) {
-            case "add":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new AddInstruction(label, r, s1, s2);
-            case "lin":
-                r = scanInt();
-                s1 = scanInt();
-                return new LinInstruction(label, r, s1);
-            case "sub":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new SubInstruction(label, r, s1, s2);
-            case "mul":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new MulInstruction(label, r, s1, s2);
-            case "div":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new DivInstruction(label, r, s1, s2);
-            case "out":
-                r = scanInt();
-                return new OutInstruction(label, r);
-            case "bnz":
-                s1 = scanInt();
-                String l2 = scan();
-                return new BnzInstruction(label, s1, l2);
-        }*/
 
         // You will have to write code here for the other instructions.
 
